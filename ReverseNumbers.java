2e436721adb6ed3043dacb866402d615e38d977e
MshallAdeel <71808687+MshallAdeel@users.noreply.github.com>
2020-09-24 09:54:16 +0430

    ReverseNumbersAndString1.3.java

--- ReverseNumbers.java
+++ ReverseNumbers.java
@@ -28,3 +28,22 @@ public class ReverseNumber {
         System.out.println("Reversed Number: " + reversed);
     }
 }
+
+    
+    // this function reverse a sentence in java
+    
+    public class Reverse {
+
+  public static void main(String[] args) {
+    String sentence = "Go work";
+    String reversed = reverse(sentence);
+    System.out.println("The reversed sentence is: " + reversed);
+  }
+
+  public static String reverse(String sentence) {
+    if (sentence.isEmpty())
+      return sentence;
+
+    return reverse(sentence.substring(1)) + sentence.charAt(0);
+  }
+}
